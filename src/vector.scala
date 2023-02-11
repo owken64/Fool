0bd3b52e268438119a753fe19393079b27aeaa95
Otonaki Kewa <30595535+owken64@users.noreply.github.com>
2023-02-12 06:05:15 +0900

    Update vector.scala

--- src/vector.scala
+++ src/vector.scala
@@ -1,2 +1,17 @@
-abstract class Vector extends Tensor
+package vector
+
+abstract class Vector[T] extends Tensor
+
+class VectorImpl[T](size: Natural) extends Vector[T] {
+    val length: Natural = size
+    val value: IndexedSeq[T] = Range(0, size.toInt) map (_=> 0)
+    def apply(index:Natural) : T = value(index.toInt)
+}
+
+object Vector {
+    def apply(size: Natural) : Vector = {
+        new VectorImpl(size)
+    }
+}
+
 
