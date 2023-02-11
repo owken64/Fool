7a024efb9e5cf3980de6f2ab76f5367d65212b54
Otonaki Kewa <30595535+owken64@users.noreply.github.com>
2023-02-12 06:05:02 +0900

    Update relation.scala

--- src/relation.scala
+++ src/relation.scala
@@ -1,1 +1,6 @@
-class Relation[T](op: (T,T)=>Boolean) extends Function[(T, T), Boolean](op)
+package relation
+
+abstract class Relation[T](op: (T,T)=>Boolean) extends Function[(T, T), Boolean](op) {
+    def apply(lhs: T, rhs:T): Boolean = op(lhs, rhs)
+}
+
