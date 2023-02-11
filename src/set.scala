9f7302d30516c7b615108bef9a1fbf1e4f87f733
Otonaki Kewa <30595535+owken64@users.noreply.github.com>
2023-02-12 06:05:10 +0900

    Update set.scala

--- src/set.scala
+++ src/set.scala
@@ -1,18 +1,18 @@
 
 package set
 
-abstract class Set[T] extends Type1{
+abstract class Set[T <: Type0] extends Type1{
     def in (obj:Any):Boolean
     def all : Seq[T]
-    def many: Seq[T]
+    def almost: Seq[T] = Range(0, 10) map (_ => generate())
+    protected def generate(): T
     override def equals(that: Any): Boolean
     val order: Num
 }
 
-class TypeSet[T] extends Set[T] {
+class TypeSet[T <: Type0] extends Set[T] {
     def in (obj:Any): Boolean = obj.isInstanceOf[T]
     def all: Seq[T] = ???
-    def many: Seq[T] = ???
     override def equals(that: Any): Boolean = that match {
         case _ : TypeSet[T] => true
         case _ => false
@@ -20,10 +20,9 @@ class TypeSet[T] extends Set[T] {
     val order:Num = ???
 }
 
-class LogicalSet[T](val predicates: Seq[Predicate]) extends Set[T] {
+class LogicalSet[T <: Type0](val predicates: Seq[Predicate]) extends Set[T] {
     def in(obj:Any):Boolean = obj.isInstanceOf[T] && predicates.forall(p => p(obj))
     def all: Seq[T] = ???
-    def many: Seq[T] = ???
     override def equals(that: Any): Boolean = that match {
         case ls : LogicalSet[T] => this.predicates == ls.predicates
         case _ => false
@@ -31,11 +30,11 @@ class LogicalSet[T](val predicates: Seq[Predicate]) extends Set[T] {
     val order:Num = ???
 }
 
-trait FiniteSet{
-
+trait FiniteSet[T <: Type0] extends Set[T] {
+    var order:Num = Natural(all.length)
 }
 
-trait InfiniteSet {
+trait InfiniteSet[T <: Type0] extends Set[T] {
     def all:Seq[T] = throw new InfiniteException
     val order: Num = Infinity
 }
@@ -45,9 +44,26 @@ object Set {
     def apply[T <: Type0]( logic: Predicate): Set[T] = new LogicalSet[T](List(logic))
 }
 
-object C extends InfiniteSet[Complex]
-object R extends InfiniteSet[Real]
-object Q extends InfiniteSet[Rational]
-object Z extends InfiniteSet[Integer]
-object N extends InfiniteSet[Natural]
-object P extends InfiniteSet[Prime]
+object C extends TypeSet[Complex] with InfiniteSet[Complex] {
+    def generate() = Complex(0,0)
+}
+
+object R extends TypeSet[Real] with InfiniteSet[Real]{
+    def generate() = Real(0)
+}
+
+object Q extends TypeSet[Rational] with InfiniteSet[Rational] {
+    def generate() = Rational(0, 1)
+}
+
+object Z extends TypeSet[Integer] with InfiniteSet[Integer] {
+    def generate() = Integer(0)
+}
+
+object N extends TypeSet[Natural] with InfiniteSet[Natural] {
+    def generate() = Natural(0)
+}
+
+object P extends TypeSet[Prime] with InfiniteSet[Prime] {
+    def generate() = Prime(2)
+}
