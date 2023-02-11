5280e022b157df0ad8598d6953adf1c9b7adf975
Otonaki Kewa <30595535+owken64@users.noreply.github.com>
2023-02-12 06:39:40 +0900

    Update vector.scala

--- src/vector.scala
+++ src/vector.scala
@@ -1,16 +1,19 @@
 package vector
 
-abstract class Vector[T] extends Tensor
+abstract class Vector[T <: Complex] extends Tensor {
+    val lenght: Natural
+    def apply(index:Natural): T
+}
 
-class VectorImpl[T](size: Natural) extends Vector[T] {
+class ZeroVector[T <: Complex](size: Natural) extends Vector[T] {
     val length: Natural = size
-    val value: IndexedSeq[T] = Range(0, size.toInt) map (_=> 0)
+    val value: IndexedSeq[T] = Range(0, size.toInt) map (_=> Compex(0, 0))
     def apply(index:Natural) : T = value(index.toInt)
 }
 
 object Vector {
-    def apply(size: Natural) : Vector = {
-        new VectorImpl(size)
+    def apply[T](size: Natural) : Vector[T] = {
+        new ZeroVector(size)
     }
 }
 
