6c35dbbfefd85bcc7c2ae36a7e9cd93ba1f1c2fc
Otonaki Kewa <30595535+owken64@users.noreply.github.com>
2023-02-12 20:50:02 +0900

    Update set.scala

--- src/set.scala
+++ src/set.scala
@@ -1,10 +1,12 @@
 
 package set
 
+import scala.math
+
 abstract class Set[T <: Type0] extends Type1{
     def in (obj:Any):Boolean
     def all : Option[Seq[T]] = None
-    def almost: Seq[T] = Range(0, 10) map (_ => generate())
+    def almost: Seq[T] = Range(0, nMany) map (_ => generate())
     protected def generate(): T
     override def equals(that: Any): Boolean
     val order: Num
@@ -42,23 +44,23 @@ object Set {
 }
 
 object C extends TypeSet[Complex] with InfiniteSet[Complex] {
-    def generate() = Complex(0,0)
+    def generate() = Complex(math.random() * 10 , math.random() * 10)
 }
 
 object R extends TypeSet[Real] with InfiniteSet[Real]{
-    def generate() = Real(0)
+    def generate() = Real(math.random() * 10)
 }
 
 object Q extends TypeSet[Rational] with InfiniteSet[Rational] {
-    def generate() = Rational(0, 1)
+    def generate() = Rational(math.float(math.random()*10).toInt, math.float(math.random()*10).toInt)
 }
 
 object Z extends TypeSet[Integer] with InfiniteSet[Integer] {
-    def generate() = Integer(0)
+    def generate() = Integer(math.float(math.random()*10).toInt)
 }
 
 object N extends TypeSet[Natural] with InfiniteSet[Natural] {
-    def generate() = Natural(0)
+    def generate() = Natural(math.float(math.random()*10).toInt)
 }
 
 object P extends TypeSet[Prime] with InfiniteSet[Prime] {
