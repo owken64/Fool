81937fedca6f4b56057a7624022786e0a05aa5ac
Otonaki Kewa <30595535+owken64@users.noreply.github.com>
2023-02-13 21:17:36 +0900

    Update set.scala

--- src/set.scala
+++ src/set.scala
@@ -1,10 +1,12 @@
 package set
 
-abstract class Set extends Type1
+abstract class Set extends Type1 {
+
+}
 
 object Set {
-    def apply(in_algorithm: Type=>Boolean): Set = new Set {
-            override def in(that:Type): Boolean = in_algorithm(that)
+    def apply(include_algorithm: Type=>Boolean): Set = new Set {
+            override def include(that:Type): Boolean = include_algorithm(that)
     }
 }
 
