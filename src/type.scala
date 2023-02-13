7e463d3fda17f434aebb0f1d52e6144d5f18f260
Otonaki Kewa <30595535+owken64@users.noreply.github.com>
2023-02-13 20:56:29 +0900

    Update type.scala

--- src/type.scala
+++ src/type.scala
@@ -3,13 +3,12 @@ package type
 abstract class Type(val t: Int){
     require( t >= 0 , "type is larger equal than 0")
     
-    def in(that:Type) :Boolean = {
-        if (this.t - that.t == 1) true
-        else false
-    }
+    def in(that:Type) :Boolean
 }
 
-abstract class Type0 extends Type(0) 
+abstract class Type0 extends Type(0) {
+    def in(that:Type) : Boolean = false
+}
 
 abstract class Type1 extends Type(1) 
 
