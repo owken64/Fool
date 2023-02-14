5c1b10d1e3f92fcb863fc9b7f1bf8abedeac568a
Otonaki Kewa <30595535+owken64@users.noreply.github.com>
2023-02-14 23:04:24 +0900

    Update function.scala

--- src/function.scala
+++ src/function.scala
@@ -1,7 +1,5 @@
 
-abstract class Function[T, U <: Num] extends Mapping[T,U] {
-    def apply(args: T): U
-}
+abstract class Function[T, U <: Num] extends Map[T,U]
 
 object AddNatural extends Function[(Natural, Natural), Natural] {
     def apply(args: (Natural, Natural)): Natural = Natural( args(0).toInt + args(1).toInt )
