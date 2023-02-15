e09fa40b9c36d131127adb2c928dedf5c6a543c7
Otonaki Kewa <30595535+owken64@users.noreply.github.com>
2023-02-15 22:07:04 +0900

    Update is.scala

--- src/is.scala
+++ src/is.scala
@@ -2,3 +2,7 @@ class Is(s: Something, t:String) extends Relation {
     val formula: String = s.toString + " is " + t
     override def eval: TruthValue = if s.typeName == t then True else False
 }
+
+object Is {
+    def apply(s:Something, t: String) : Is = new Is(s, t)
+}
