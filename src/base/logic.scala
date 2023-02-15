deb5d1d528a18d7953f8f5d4ef0d39f762c56eeb
Otonaki Kewa <30595535+owken64@users.noreply.github.com>
2023-02-15 20:59:45 +0900

    Create logic.scala

---
+++ src/base/logic.scala
@@ -0,0 +1,27 @@
+package logic
+
+import truthValue
+import something
+import exception
+
+abstract class LogicalFormula {
+    def name: Option[String] = None
+    val formula: String
+    def eval: TruthValue = Unsolved
+
+    override def toString(): String = formula
+}
+
+class AssignedPredicate(n: String, val p: Something => TruthValue, val s: Something) extends LogicalFormula {
+    override def name: Option[String] = Some(n)
+    val formula: String = name.get + "(" + s.toString + ")"
+    override def eval: TruthValue = p(s)
+}
+
+class Predicate(n: String, val p: Something => TruthValue) extends LogicalFormula {
+    override def name: Option[String] = Some(n)
+    val formula: String = name.get + "(x)"
+    override def eval: TruthValue = throw new NotAssignedException
+    def apply(s: Something) : AssignedPredicate  = new AssignedPredicate(n, p, s)
+} 
+
