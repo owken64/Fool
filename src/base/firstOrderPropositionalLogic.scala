897f4061619d962f056f9e4f1416d53793387010
Otonaki Kewa <30595535+owken64@users.noreply.github.com>
2023-02-23 09:53:57 +0900

    Update firstOrderPropositionalLogic.scala

--- src/base/firstOrderPropositionalLogic.scala
+++ src/base/firstOrderPropositionalLogic.scala
@@ -1,9 +1,26 @@
 package logic.firstOrderPropositionalLogic
 
+import logic.LogicalFormula
+
 package syntax {
 
 }
 
 package semantics {
+    abstract class Dt
+    object One extends Dt
+    object Zero extends Dt
 
+    class 解釈(I: LogicalFormula => Dt) {
+        def apply(lf: LogicalFormula) = I(lf)
+    }
+
+    var allI: List[解釈] = Nil
+}
+
+package inference {
+
+    class Inference(gamma: Seq[LogicalFormula], delta: Option[LogicalFormula]) {
+        require( !semantics.aiiI.exists( I => gamma.forall( x => I(x)) && if ( !delta.isEmpty ) I(delta.get) == semantics.Zero )  )
+    }
 }
