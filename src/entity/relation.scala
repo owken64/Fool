31d848d4f16717fcc4bdb91ad80d3c6fd25e5a26
Otonaki Kewa <30595535+owken64@users.noreply.github.com>
2023-03-06 22:39:11 +0900

    Create relation.scala

---
+++ src/entity/relation.scala
@@ -0,0 +1,13 @@
+package entity
+
+class Relation extends Map
+
+object in extends Relation {
+    def apply(lhs:Any, rhs:Any): Boolean = {
+        rhs match {
+            case ls: LogicalSet => ls.condition(lhs)
+            case es: ElementalSet => es.elements.exists(x => x == lhs)
+            case _ => false
+        }
+    }
+}
