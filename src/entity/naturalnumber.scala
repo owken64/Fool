782e2dd92a847f0bfc45ee37544c6f46d0aee307
Otonaki Kewa <30595535+owken64@users.noreply.github.com>
2023-03-06 22:46:26 +0900

    Update naturalnumber.scala

--- src/entity/naturalnumber.scala
+++ src/entity/naturalnumber.scala
@@ -1,6 +1,20 @@
 package entity
 
-class NaturalNumber(val n: Int) extends Number {
-    require( n >= 0 )
+abstract class NaturalNumber extends Number
+
+class SUCC(expr: NaturalNumber) extends NaturalNumber {
+    override def toString: String = "SUCC(" + expr.toString +")"
 }
 
+object Zero extends NaturalNumber {
+    override def toString: String = "0"
+}
+
+object NaturalNumber {
+    def apply(n: Int): NaturalNumber = {
+        n match {
+            case 0 => Zero
+            case n if n > 0 => new SUCC( NaturalNumber(n-1) )
+        }
+    }
+}
